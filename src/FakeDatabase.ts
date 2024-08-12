a30ac837d258e60da14f2fd24c26ebfa221b6b8a
Bhagya5467 <ebdivyanjalee@gmail.com>
2024-08-12 20:08:38 +0530

    first commit

---
+++ src/FakeDatabase.ts
@@ -0,0 +1,75 @@
+export interface Book {
+  id: number;
+  title: string;
+  author: string;
+  publicationYear: number;
+}
+
+export const books: Book[] = [
+  {
+    id: 1 ,
+    title: 'The Famous Five',
+    author: 'Enid Blyton',
+    publicationYear: 1960,
+  },
+  {
+    id: 2 ,
+    title: 'Gamperaliya',
+    author: 'Martin Wickramasinghe',
+    publicationYear: 1967,
+  },
+  {
+    id: 3 ,
+    title: 'May Maara Prasangaya',
+    author: 'Mahinda Masimbula',
+    publicationYear: 2023,
+  },
+  {
+    id: 4 ,
+    title: 'Forget me not',
+    author: 'Gayani & Suvini',
+    publicationYear: 2023,
+  },
+  {
+    id: 5 ,
+    title: 'Senkottan',
+    author: 'Masimbula',
+    publicationYear: 2017,
+  },
+  {
+    id: 6 ,
+    title: 'Thun man handiya',
+    author: 'Mahagama Sekara',
+    publicationYear: 1970 ,
+  },
+  {
+    id: 7 ,
+    title: 'The village by the sea',
+    author: 'Anita Desai',
+    publicationYear: 2004,
+  },
+  {
+    id: 8 ,
+    title: 'Poliyana',
+    author: 'Kathyana Amarasinghe',
+    publicationYear: 2010,
+  },
+  {
+    id: 9 ,
+    title: 'Secret Seven',
+    author: 'Enid Blyton',
+    publicationYear: 1956,
+  },
+  {
+    id: 10 ,
+    title: 'Malory towers',
+    author: 'Enid Blyton',
+    publicationYear: 1958,
+  },
+  {
+    id: 11 ,
+    title: 'Harry Potter',
+    author: 'JK Rollin',
+    publicationYear: 2000,
+  },
+]
